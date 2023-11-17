044048c6ced0625c3b902c6c159cf43c0def7b41
luissancar <luissancar@yahoo.es>
2023-11-17 20:15:32 +0100

    FloatingActionButton

--- app/src/main/java/com/example/scaffoldtopappsnackbaretc/MainActivity.kt
+++ app/src/main/java/com/example/scaffoldtopappsnackbaretc/MainActivity.kt
@@ -22,25 +22,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    PrintBottomBar()
+                    PrintFloatingButtonBar()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ScaffoldTopAppSnackbaretcTheme {
-        Greeting("Android")
     }
 }
